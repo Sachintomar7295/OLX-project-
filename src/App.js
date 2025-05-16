4246cb768b512b1e1c947f60293cede1ba356d51
Your Name <youremail@example.com>
2025-05-16 14:34:10 +0530

    Push working React OLX project

---
+++ src/App.js
@@ -0,0 +1,16 @@
+
+// src/App.jsx
+import React from 'react';
+import CarForm from './components/CarForm';
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <CarForm />
+    </div>
+  );
+}
+
+export default App;
